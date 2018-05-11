42012eef40ed8866def01c7a2e94eb60b9cd0c94
Jarrod Sawyer <jarrod.sawyer@gmail.com>
2018-05-11 11:36:16 -0500

    Add SB-Admin 2 theme. Make default page Login Organize Layout

--- Workout Planner/Controllers/HomeController.cs
+++ Workout Planner/Controllers/HomeController.cs
@@ -5,9 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Workout_Planner.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Workout_Planner.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public IActionResult Index()
